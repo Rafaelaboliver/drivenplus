147e21c9a8a166df2ddd80cb7048c579c047d036
rafaelaboliver <rafaelaa.oliveiraa.rb@gmail.com>
2022-12-21 19:51:22 -0300

    projeto com as páginas e componentes organizados em pastas.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from '../pages/LoginPage/LoginPage'
+import SignUpPage from '../pages/SignUpPage/SignUpPage'
+import SubscriptionsPage from '../pages/SubscriptionsPage'
+import HomePage from '../pages/HomePage/HomePage'
 
 export default function App() {
   return (
-    <div >
-      
-    </div>
+   
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+        <Route path="/sign-up" element={<SignUpPage/>}/>
+        <Route path="/subscriptions" element={<SubscriptionsPage/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+      </Routes>
+      </BrowserRouter>
+  
   );
 }
 
